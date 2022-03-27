11b7188ae702970f08766b726004022c94cd85cd
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-03-27 11:40:44 -0700

    create routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,22 +1,59 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+      },
+      {
+        through: ProductTag,
+        model: Tag,
+      },
+    ],
+  })
+    .then((rows) => {
+      res.json(rows);
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: err });
+    });
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const id = req.params.id;
+  Product.findOne({
+    where: {
+      id,
+    },
+    include: [
+      {
+        model: Category,
+      },
+      {
+        through: ProductTag,
+        model: Tag,
+      },
+    ],
+  })
+    .then((rows) => {
+      res.json(rows);
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: err });
+    });
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +85,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,8 +126,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  const id = req.params.id;
+  Product.destroy({
+    where: { id },
+  })
+    .then((isDeleted) => {
+      res.status(200).send("" + isDeleted);
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: err });
+    });
 });
 
 module.exports = router;
